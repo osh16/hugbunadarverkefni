38f94a142e02e2f8d1f524faf2eeeb115a3902cd
Oskar Hoskuldsson <osh16@hi.is>
2020-10-08 13:15:44 +0000

    fix dependencies in pom.xml, user entity added

--- Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Entities/User.java
+++ Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Entities/User.java
@@ -8,4 +8,26 @@ import javax.persistence.Id;
 @Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String name;
+	private String password;
+
+	// Viljum ad thetta se sql date format
+	// * private Date signupdate;
+	// * private Date lastonline;
+
+	// Viljum bua til eh eins og:
+	// * private Messages messages
+	// * private Friends friends
+	// * private Comments comments
+	// * private Threads threads
+
+	public User(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
 }
