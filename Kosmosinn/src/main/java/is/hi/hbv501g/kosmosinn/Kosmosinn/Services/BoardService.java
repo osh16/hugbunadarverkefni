ac1c756306c67ce08c9bb8ff946b7ab4844669cb
ass106 <43350367+ass106@users.noreply.github.com>
2020-10-23 16:55:59 +0000

    BoardService merged from oskar

--- Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Services/BoardService.java
+++ Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Services/BoardService.java
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-package is.hi.hbv501g.kosmosinn.Kosmosinn.Services;
-import is.hi.hbv501g.kosmosinn.Kosmosinn.Entities.Board;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface BoardService {
-    Board save(Board board);
-    void delete(Board board);
-    List<Board> findAll();
-    List<Board> findByBoardname(String boardname);
-    Optional<Board> findById(long id);
-}
-=======
 package is.hi.hbv501g.kosmosinn.Kosmosinn.Services;
 
 import is.hi.hbv501g.kosmosinn.Kosmosinn.Entities.Board;
@@ -27,4 +12,3 @@ public interface BoardService {
     Optional<Board> findById(long id);
     Board findByName(String name);
 }
->>>>>>> origin/oskar_branch
