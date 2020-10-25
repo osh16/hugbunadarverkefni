066f5e5c0de2d4657bc9da4ea4d89bf27c97bf02
vsv8 <vsv8@hi.is>
2020-10-25 19:15:32 +0000

    .

--- Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Controllers/CommentController.java
+++ Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Controllers/CommentController.java
@@ -8,9 +8,7 @@ import org.apache.catalina.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
@@ -27,15 +25,10 @@ public class CommentController {
         this.userService = userService;
         this.topicService = topicService;
     }
-
-    @RequestMapping(value="@{/topic/{id}(id=${topic.id})}", method = RequestMethod.POST)
-    public String createComment(@Valid Comment comment, BindingResult result, Model model, long id) {
+    /*
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public String createComment(@Valid @ModelAttribute Comment comment, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            System.out.println("createcomment post error");
-            System.out.println("createcomment post error");
-            System.out.println("createcomment post error");
-            System.out.println("createcomment post error");
-            System.out.println("createcomment post error");
             System.out.println("createcomment post error");
             return "redirect:/topic/{id}(id=${topic.id})}";
         }
@@ -43,16 +36,27 @@ public class CommentController {
         model.addAttribute("comments", commentService.findAll());
         //model.addAttribute("users", userService.findAll());
         //model.addAttribute("topics",topicService.findById(id).orElseThrow(()-> new IllegalArgumentException("Invalid id")));
-
         System.out.println("createcomment post");
 
-        return "redirect://topic/{id}(id=${topic.id})}";
+        return "redirect:/topic/{id}(id=${topic.id})}";
     }
 
     @RequestMapping(value="@{/topic/{id}(id=${topic.id})}", method = RequestMethod.GET)
-    public String createCommentForm(Comment comment, @PathVariable long topicId) {
+    public String createCommentForm(Comment comment) {
         System.out.println("createcomment get");
         return "topic-content";
+    }*/
+    @PostMapping("addcommentData")
+    public String createComment(@ModelAttribute Comment comment, Model model) {
+        commentService.save(comment);
+        return "redirect:/topic-content";
+    }
+
+    @GetMapping("/addcomment")
+    public String commentPage(Model model) {
+        Comment comment = new Comment();
+        model.addAttribute("comment", comment);
+        return "addcomment";
     }
 
 }
