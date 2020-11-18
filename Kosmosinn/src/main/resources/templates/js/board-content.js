64be3f957e1bf47acadd6ee31d4ba56b1079d8b3
Oskar Hoskuldsson <osh16@hi.is>
2020-11-18 22:03:02 +0000

    push fyrir nytt branch

---
+++ Kosmosinn/src/main/resources/templates/js/board-content.js
@@ -0,0 +1,10 @@
+<script>
+    $(document).ready(function() {
+        var id;
+        var tmp;
+        $(".edit-topic").click(function() {
+            id = this.getAttribute("id").substr(5);
+            $("#topic-"+id).load("topic/" + id + "/edit");
+        });
+    });
+</script>
