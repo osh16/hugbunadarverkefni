cfe220b6b832f59789950474a0a6c2a921623804
ass106 <43350367+ass106@users.noreply.github.com>
2020-11-18 22:40:38 +0000

    CSS underway

--- Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Controllers/TopicController.java
+++ Kosmosinn/src/main/java/is/hi/hbv501g/kosmosinn/Kosmosinn/Controllers/TopicController.java
@@ -15,7 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.soap.SOAPBinding;
+
 import javax.naming.Binding;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
